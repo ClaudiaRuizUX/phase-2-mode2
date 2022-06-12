c5ead1400f53054b79e14d636863f89147a0fd53
Claudia <claudiaruizux@gmail.com>
2022-06-12 14:47:14 +1000

    handles error and adds loading state

--- src/apis/news.js
+++ src/apis/news.js
@@ -1,6 +1,5 @@
 
 // const weeksAgo = xxx;
-
 export function getArticleListAPI(subject) {
     return fetch(`https://newsapi.org/v2/everything?q=${subject}&from=2022-05-25&sortBy=publishedAt&apiKey=ccf2290e603243e3abf153def8f8ce3f`)
       .then(res => res.json())
--- src/components/NewsCards.js
+++ src/components/NewsCards.js
@@ -7,26 +7,38 @@ const NewsCards = (props) => {
   const [articleList, setArticleList] = useState([]);
   const [savedList, setSavedList] = useState([]);
   const {subject} = props;
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
 
   useEffect(() => {
+    setLoading(true);
     getArticleListAPI(subject)
-      .then(articleList => setArticleList(articleList));
-  },[])
+      .then(articleList => setArticleList(articleList))
+      .then(() => setLoading(false))
+      .catch(setError)
+   },[subject])
 
   useEffect(() => {
+    setLoading(true);
     getSavedListAPI()
-      .then(savedList => setSavedList(savedList));
+      .then(savedList => setSavedList(savedList))
+      .catch(setError)
   },[])
+
+
 
   const deleteArticle = (id) => {
+    setLoading(true);
     return deleteSavedArticleAPI(id)
     .then(() => {
       setSavedList(savedList.filter(savedArticle => savedArticle.id !== id))
+      .catch(setError)
     })
   }
 
   const saveArticle = (article) => {
+    setLoading(true);
     return fetch('http://localhost:3002/savedArticles', {
           method: 'POST',
           headers: {
@@ -40,29 +52,42 @@ const NewsCards = (props) => {
       })
       .then (getSavedListAPI()
       .then(savedList => setSavedList(savedList)))
+      .catch(setError)
   };
 
+  if (loading) return <h1> Loading...</h1>;
+
+  if (error) {
+    return <pre>{JSON.stringify(error, null, 2)}</pre>;
+  }
+
+
+  if (articleList) {
     return (
-  <>
-    <h3>{subject} news</h3>  
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col s8 cards-container">
-            {articleList.map((article =>
-                <NewsArticle 
-                key={article.url} title={article.title} img={article.urlToImage}
-                url={article.url} date={article.publishedAt} saveArticle={saveArticle}/>
-                ))
-            }
+      <>
+        <h3>{subject} news</h3>  
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col s8 cards-container">
+                {articleList.map((article =>
+                    <NewsArticle 
+                    key={article.url} title={article.title} img={article.urlToImage}
+                    url={article.url} date={article.publishedAt} saveArticle={saveArticle}/>
+                    ))
+                }
+              </div>
+              <div className="col s4">
+                <h4><i className="material-icons small left">bookmark_border</i>Bookmarks</h4>
+                <BookmarkCards savedList={savedList} deleteArticle={deleteArticle} />
+              </div>
+            </div>
           </div>
-          <div className="col s4">
-            <h4><i className="material-icons small left">bookmark_border</i>Bookmarks</h4>
-            <BookmarkCards savedList={savedList} deleteArticle={deleteArticle} />
-          </div>
-        </div>
-      </div>
-  </>
-    );
+      </>
+      );
+  }
+
+    return <div>No Data Available</div>;
+    
 };
 
 export default NewsCards;
