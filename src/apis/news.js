5010799085bd3f9e1d219f6f32b5566ba1334813
ClaudiaRuizUX <claudiaruizux@gmail.com>
2022-06-15 18:57:58 +1000

    adds error handling to news api

--- src/apis/news.js
+++ src/apis/news.js
@@ -1,20 +1,12 @@
 
 
-
-
 export function getArticleListAPI(subject) {
   return fetch(`https://newsapi.org/v2/${subject}&apiKey=ccf2290e603243e3abf153def8f8ce3f`)
   // return fetch(`http://localhost:3001/articles`)
     .then(res => res.json())
-    .then(data => data.articles);
+    .then(data => data.articles)
+    .catch((error)=> error);
 };
-
-
-// export function getArticleListAPI(subject) {
-//     return fetch(`https://newsapi.org/v2/everything?q=${subject}&from=${daysAgo18}&sortBy=publishedAt&apiKey=ccf2290e603243e3abf153def8f8ce3f`)
-//       .then(res => res.json())
-//       .then(data => data.articles);
-// };
 
 export function getSavedListAPI() {
     return fetch('http://localhost:3002/savedArticles')
